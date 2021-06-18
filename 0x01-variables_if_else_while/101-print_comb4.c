6def09050ae6c94b569d55136b30004a296c0b20
ABUBACARSC <sayidcassim0@gmail.com>
2021-06-18 13:35:24 -0700

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 /**
+* main - Prints multiple combinations
+* Description: Write a program that prints all possible
+* Return: 0
 *Just check the code and undeestand it
 *no more description
 */
