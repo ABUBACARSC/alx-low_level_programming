f8a742f15997b8a229b0bbc648cde6403520d8ec
ABUBACARSC <sayidcassim0@gmail.com>
2021-06-22 11:03:48 -0700

    ksh

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 /**
-*  main - check the code for Holberton School students.
+*main:check the code for Holberton School students.
 * 
-*  Return: Always 0.
+*Return: Always 0.
 *     
 */
-
 int main(void)
 {
 char holberton[9] = "Holberton";
