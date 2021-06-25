8663736e6d729356a04667d4f95b06c7907735e6
ABUBACARSC <sayidcassim0@gmail.com>
2021-06-24 19:16:54 -0700

    half the tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*  * more_numbers - Print numbers between 0 to 14 incl.
+*   *
+*    * Return: Void.
+*/
+
+
+void more_numbers(void)
+
+{
+int i, j;
+for (int i=0; i<10; i++)
+{
+for (j=0; j<10; j++)
+{
+_putchar(j)
+}
+_putchar("/n")
+}
+}
+
