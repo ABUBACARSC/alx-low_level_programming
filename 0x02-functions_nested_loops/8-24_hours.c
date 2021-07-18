776b5401afe3789f7404b714907dfef4cab7d087
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-18 02:21:29 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@
 void jack_bauer(void)
 {
 int hour, minute;
-for(hour =0; hour<24; hour++)
+for(hour=0; hour<24; hour++)
 {
 for(minute=0; minute<=60; minute++)
 {
@@ -19,5 +19,6 @@ _putchar(minute);
 _putchar('\n');
 }
 }
+return;
 }
 
