3bb282f2aa9b4d37d713577e2ac3de1194288fb4
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-18 04:48:00 -0700

    done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_to_98 - prints last digit of number
+* @n: number to print last digit of
+*
+* Return: the last digit as an int
+*/
+
+void print_times_table(int n)
+{
+int a, b, c, d;
+d = n - 1;
+if (a >= 0 && a <= 15)
+{
+for ( a = 0; a <= n; a++)
+{
+for (b = 0; b <= d; b++)
+{
+c = a * b;
+_putchar(99);
+_putchar(44);
+_putchar(32);
+}
+int e = a * n;
+_putchar(101);
+_putchar('\n')
+}
+}
+}
+
