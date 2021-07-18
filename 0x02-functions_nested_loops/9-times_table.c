4fe6364d678de84822defe8d7f45da17c75aa056
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-18 02:53:56 -0700

    std used

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,4 @@
+#include <stdio>
 #include "holberton.h"
 /**
 *  *times_table- prints every minute of the day of Jack Bauer,
@@ -9,12 +10,12 @@
 void times_table(void)
 {
 int n, x, y;
-for (n = 0; n<=9; n++)
+for (n = 0; n <= 9; n++)
 {
 for (x = 0; x <= 9; x++)
 {
-y = x*n;
-_putchar(y);
+y = x * n;
+putchar(y);
 _putchar(',');
 }
 _putchar('\n');
