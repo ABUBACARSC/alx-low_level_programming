6bc3e6bf587731aef8b0f508ad8aa2dde239a5af
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-19 02:18:51 -0700

    add numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_times_table - prints last digit of number
+* @n: number to print last digit of
+* Return: the last digit as an int
+*/
+void main (void)
+{
+int a;
+a = 0;
+for (n = 0; n <= 1023; n++)
+{
+if (n % 3 == 0 && n % 5 = 0)
+{
+a = a + n;
+}
+}
+printf("%d", a);
+}
+
