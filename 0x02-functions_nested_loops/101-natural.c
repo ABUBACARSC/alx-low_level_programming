62bd3fd9c57e0e852eaaaff8752ebceeb3565957
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-19 03:01:54 -0700

    done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* print_times_table - prints last digit of number
+* print_natural:  - prints last digit of number
+* main: calls his friend
 * @n: number to print last digit of
 * Return: the last digit as an int
 */
@@ -15,15 +16,17 @@ return (0);
 
 int print_natural(int n)
 {
-int a, b, c, d;
+int a, b, c, d, e;
 a = 0;
+e = 0;
 d = 0;
 for (c = 0; c <= n; c++)
 {
 b = c;
 if (b % 3 == 0 && b % 5 == 0)
 {
-a = a + b;
+e = b;
+a = a + e;
 }
 else
 {
