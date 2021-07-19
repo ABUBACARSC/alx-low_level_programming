f5dc155c4a195160baf64799bb056d68d628fa4b
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-19 02:45:10 -0700

    done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,22 +5,30 @@
 * @n: number to print last digit of
 * Return: the last digit as an int
 */
+char print_natural(int n);
+
 int main (void)
 {
-int a, b, n;
-a = 0;
-for (n = 0; n <= 1023; n++)
+print_natural(1023);
+return(0);
+}
+
+char print_natural(int n)
 {
-b = n;
+int a, b, c, d;
+a = 0;
+for (c = 0; c <= 1023; c++)
+{
+b = c;
 if (b%3 == 0 && b%5 = 0)
 {
-a = a + n;
+a = a + b;
 }
 else
 {
-b = b;
+d = d + b;
 }
 printf("%d", a);
-return(a);
+return("SUCCESS");
 }
 
