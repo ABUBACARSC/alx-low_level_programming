7e30a4eab6c0ea7f30ee3d0a9b88a217faa0209a
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-19 01:58:07 -0700

    sapces solved

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,16 +13,16 @@ int c, a, b, e, d;
 d = n - 1;
 if (n >= 0 && n <= 15)
 {
-for ( a = 0; a <= n; a++)
+for (a = 0; a <= n; a++)
 {
 for (b = 0; b <= d; b++)
 {
 c = a * b;
-printf("%d, ", c);
+printf("%d,   ", c);
 }
 e = a * n;
 printf("%d\n", e);
 }
 }
 }
- 
+
