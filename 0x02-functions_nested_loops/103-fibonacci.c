e833b528b4a3df0eaa62ab2db5145a8cdf25bb51
ABUBACARSC <sayidcassim0@gmail.com>
2021-07-19 13:40:08 -0700

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,20 +5,23 @@
 */
 int main(void)
 {
-int counter;
-counter =0;
 unsigned long a, b, c, d;
 a = 1;
 b = 2;
-
+c = 0;
+d = 0;
 printf ("%ld, %ld, ", a, b);
 while (c <= 4000000)
 {
 c = a + b;
-printf("%ld, ", c)
+if (c % 2 == 0)
+{
+d = d + c;
+}
 a = b;
 b = c;
 }
+printf("%d\n", d);
 return (0);
 }
 
